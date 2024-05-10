72c876ec1a218fbf1b725ddd28b8c2f48cbe4bae
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-05-10 11:15:13 +0000

    Add boxicons stylesheet

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -16,6 +16,12 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
+      <head>
+        <link
+          href="https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css"
+          rel="stylesheet"
+        ></link>
+      </head>
       <body className={inter.className}>{children}</body>
     </html>
   );
