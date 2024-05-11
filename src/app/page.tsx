af1ad39bd71cde1d196477b22c32c367cd8d5c69
Michael Quarcoo <michaelquarcoo04@gmail.com>
2024-05-11 06:46:24 +0000

    Modify landing page content

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,11 +2,12 @@ import { Icon } from "@/components";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between py-24">
-      <div className="flex gap-3 justify-center text-center">
-        <h2 className="text-3xl">We go</h2>
-        <Icon name="bx-rocket" size="36px" />
-        <Icon name="bx-rocket" size="36px" />
+    <main className="flex min-h-screen flex-col items-center justify-center py-24 gap-3">
+      <Icon name="bx-run" size="108px" />
+      <h2 className="text-6xl">We go</h2>
+      <div className="flex mt-4 gap-3">
+        <Icon name="bx-rocket" size="64px" />
+        <Icon name="bx-rocket" size="64px" />
       </div>
     </main>
   );
